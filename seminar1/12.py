f3bbd0f99f209847bd20e7ce7a776e9140053a78
Verina Olga <verinaolga@gmail.com>
2022-03-12 20:22:09 +0300

    12 task updated second time

--- seminar1/12.py
+++ seminar1/12.py
@@ -2,6 +2,5 @@
 
 number = int((input('Enter the number: ')))
 
-print('The new number is', int(number/100), int(number%10))
-print()
-print(f'The new number is {number//100}{int(number%10)}')
+print(f'The new number is {number//100 * 10 + int(number%10)}')
+
