36c90486e5da2d698ceed4f1abc98cc40aa142e0
Verina Olga <verinaolga@gmail.com>
2022-03-12 21:36:08 +0300

    rearranging tasks

--- seminar1/14.py
+++ seminar1/14.py
@@ -2,7 +2,8 @@
 
 number = int(input('Enter the number:'))
 
-if number // 100 > 0:
+
+if number // 100 != 0:
     print (int(number / 100 % 10))
 else:
     print ('The third letter does not exists')
